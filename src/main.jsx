db89ec59793b7c3f75d03c6ae753635e9be5c174
ardi667 <149851777+ardi667@users.noreply.github.com>
2026-01-09 11:54:09 +0700

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// Mencari elemen dengan id 'root' di index.html dan merender aplikasi React di sana
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
